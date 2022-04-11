d807e43d5e4080250bf64af6711ca3d0a539ea77
Sean Hwang <hwang103@purdue.edu>
2022-04-11 14:56:21 -0400

    System Controller Setup

--- detect.py
+++ detect.py
@@ -17,7 +17,7 @@ Usage - formats:
                                          yolov5s.onnx               # ONNX Runtime or OpenCV DNN with --dnn
                                          yolov5s.xml                # OpenVINO
                                          yolov5s.engine             # TensorRT
-                                         yolov5s.mlmodel            # CoreML (macOS-only)
+                                         yolov5s.mlmodel            # CoreML (MacOS-only)
                                          yolov5s_saved_model        # TensorFlow SavedModel
                                          yolov5s.pb                 # TensorFlow GraphDef
                                          yolov5s.tflite             # TensorFlow Lite
@@ -26,11 +26,17 @@ Usage - formats:
 
 import argparse
 import os
+import os.path
 import sys
 from pathlib import Path
+import numpy as np
 
+import cv2
+import csv
+from time import time  
 import torch
 import torch.backends.cudnn as cudnn
+from datetime import datetime
 
 FILE = Path(__file__).resolve()
 ROOT = FILE.parents[0]  # YOLOv5 root directory
@@ -40,15 +46,14 @@ ROOT = Path(os.path.relpath(ROOT, Path.cwd()))  # relative
 
 from models.common import DetectMultiBackend
 from utils.datasets import IMG_FORMATS, VID_FORMATS, LoadImages, LoadStreams
-from utils.general import (LOGGER, check_file, check_img_size, check_imshow, check_requirements, colorstr, cv2,
+from utils.general import (LOGGER, check_file, check_img_size, check_imshow, check_requirements, colorstr,
                            increment_path, non_max_suppression, print_args, scale_coords, strip_optimizer, xyxy2xywh)
 from utils.plots import Annotator, colors, save_one_box
 from utils.torch_utils import select_device, time_sync
 
 
 @torch.no_grad()
-def run(
-        weights=ROOT / 'yolov5s.pt',  # model.pt path(s)
+def run(weights=ROOT / 'yolov5s.pt',  # model.pt path(s)
         source=ROOT / 'data/images',  # file/dir/URL/glob, 0 for webcam
         data=ROOT / 'data/coco128.yaml',  # dataset.yaml path
         imgsz=(640, 640),  # inference size (height, width)
@@ -74,8 +79,9 @@ def run(
         hide_conf=False,  # hide confidences
         half=False,  # use FP16 half-precision inference
         dnn=False,  # use OpenCV DNN for ONNX inference
-):
+        ):
     source = str(source)
+    #print(source)
     save_img = not nosave and not source.endswith('.txt')  # save inference images
     is_file = Path(source).suffix[1:] in (IMG_FORMATS + VID_FORMATS)
     is_url = source.lower().startswith(('rtsp://', 'rtmp://', 'http://', 'https://'))
@@ -89,9 +95,54 @@ def run(
 
     # Load model
     device = select_device(device)
-    model = DetectMultiBackend(weights, device=device, dnn=dnn, data=data, fp16=half)
-    stride, names, pt = model.stride, model.names, model.pt
+    model = DetectMultiBackend(weights, device=device, dnn=dnn, data=data)
+    stride, names, pt, jit, onnx, engine = model.stride, model.names, model.pt, model.jit, model.onnx, model.engine
     imgsz = check_img_size(imgsz, s=stride)  # check image size
+
+    # Code inserted here:
+    # user selection for food and water bowl:
+    boxes = []
+    # Read image
+    img = cv2.VideoCapture(source)
+    ret, frame = img.read()
+    # frame = cv2.resize(frame, None, fx = 0.3045,fy = 0.5778)
+
+    while(len(boxes) < 2):
+        frame_cpy = frame.copy()
+        if(len(boxes) == 0): 
+            im0 = cv2.rectangle(frame_cpy, (0,0), (600, 80) , (0,0,0), -1)
+            im0 = cv2.putText(frame_cpy, 'Select Water Bowl', (10, 60), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 0, 255), 2, cv2.LINE_AA)
+            print('Water Bowl Selection')
+        elif(len(boxes) == 1): 
+            im0 = cv2.rectangle(frame_cpy, (0,0), (570, 80) , (0,0,0), -1)
+            im0 = cv2.putText(frame_cpy, 'Select Food Bowl', (10, 60), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 0, 255), 2, cv2.LINE_AA)
+            print('Food Bowl Selection')
+        # Select ROI
+        r = cv2.selectROI(frame_cpy, fromCenter=False, showCrosshair=False) # returns (x, y, w, h)
+        # Bounded image
+        boxes.append(((int(r[0]),int(r[1])), (int(r[0]+r[2]),int(r[1]+r[3]))))
+        print(boxes[-1])
+        cv2.waitKey(1)
+    cv2. destroyAllWindows()
+
+    # frame.shape[0] = height and frame.shape[1] = width
+    '''
+    normBoxes = ((
+        (boxes[0][0][0] / frame.shape[1], boxes[0][0][1] / frame.shape[0]), 
+        (boxes[0][1][0] / frame.shape[1], boxes[0][1][1] / frame.shape[0])),
+        ((boxes[1][0][0] / frame.shape[1], boxes[1][0][1] / frame.shape[0]), 
+        (boxes[1][1][0] / frame.shape[1], boxes[1][1][1] / frame.shape[0])))
+
+    normBoxes = ((((boxes[0][0][0] + boxes[0][1][0]) / (2 * frame.shape[1])), ((boxes[0][0][1] + boxes[0][1][1]) / (2 * frame.shape[0])), (boxes[0][1][0] - boxes[0][0][0]) / frame.shape[1], (boxes[0][1][1] - boxes[0][0][1]) / frame.shape[0]),
+                 (((boxes[1][0][0] + boxes[1][1][0]) / (2 * frame.shape[1])), ((boxes[1][0][1] + boxes[1][1][1]) / (2 * frame.shape[0])), (boxes[1][1][0] - boxes[1][0][0]) / frame.shape[1], (boxes[1][1][1] - boxes[1][0][1]) / frame.shape[0]))
+    print(normBoxes)
+    '''
+    #inserted code ends here.
+
+    # Half
+    half &= (pt or jit or onnx or engine) and device.type != 'cpu'  # FP16 supported on limited backends with CUDA
+    if pt or jit:
+        model.model.half() if half else model.model.float()
 
     # Dataloader
     if webcam:
@@ -105,12 +156,15 @@ def run(
     vid_path, vid_writer = [None] * bs, [None] * bs
 
     # Run inference
-    model.warmup(imgsz=(1 if pt else bs, 3, *imgsz))  # warmup
+    model.warmup(imgsz=(1 if pt else bs, 3, *imgsz), half=half)  # warmup
     dt, seen = [0.0, 0.0, 0.0], 0
+    prev_seen, prev_start_point, prev_end_point = 0, None, None
+    activity = None
+    timestamp = 0
     for path, im, im0s, vid_cap, s in dataset:
         t1 = time_sync()
         im = torch.from_numpy(im).to(device)
-        im = im.half() if model.fp16 else im.float()  # uint8 to fp16/32
+        im = im.half() if half else im.float()  # uint8 to fp16/32
         im /= 255  # 0 - 255 to 0.0 - 1.0
         if len(im.shape) == 3:
             im = im[None]  # expand for batch dim
@@ -146,8 +200,18 @@ def run(
             gn = torch.tensor(im0.shape)[[1, 0, 1, 0]]  # normalization gain whwh
             imc = im0.copy() if save_crop else im0  # for save_crop
             annotator = Annotator(im0, line_width=line_thickness, example=str(names))
+            iou = 0
+            unNorm_xyxy = [[0,0,0,0]]
+            iou_water, iou_food = 0, 0
             if len(det):
                 # Rescale boxes from img_size to im0 size
+
+                ######### code added here:
+                # save bounding box info to a varaible.
+                #print(det[:, :4])
+
+                ###### code ends here:
+
                 det[:, :4] = scale_coords(im.shape[2:], det[:, :4], im0.shape).round()
 
                 # Print results
@@ -158,21 +222,181 @@ def run(
                 # Write results
                 for *xyxy, conf, cls in reversed(det):
                     if save_txt:  # Write to file
+                        unNorm_xyxy = torch.tensor(xyxy).view(1, 4) # Pixel values of cat bounding box upper corner x & y, lower corner x & y
+                        print(unNorm_xyxy)
                         xywh = (xyxy2xywh(torch.tensor(xyxy).view(1, 4)) / gn).view(-1).tolist()  # normalized xywh
                         line = (cls, *xywh, conf) if save_conf else (cls, *xywh)  # label format
+
+                        ### code added here:
+                        #print(xywh[1])
+                        #print(cls.item())
+                        #print(line[1], line[2], line[3], line[4])
+                        bounding_box_info = cls.item(), xywh
+                        #print(bounding_box_info)
+
+                        # iou calculations:
+                        # bounding box info for the cat:
+                        x1 = unNorm_xyxy[0][0]
+                        x2 = unNorm_xyxy[0][2]
+                        y1 = unNorm_xyxy[0][1]
+                        y2 = unNorm_xyxy[0][3]
+                        #print(x1,y1)
+                        #print(x2,y2)
+                        area_cat_box = (x2-x1)*(y2-y1)
+                        width_waterBowl = boxes[0][1][0]-boxes[0][0][0]
+                        height_waterBowl = boxes[0][1][1]-boxes[0][0][1]
+                        area_water_box = width_waterBowl * height_waterBowl
+                        xleft_water = max(x1,boxes[0][0][0])
+                        xright_water = min(x2, boxes[0][1][0])
+                        ytop_water = max(y1, boxes[0][0][1])
+                        ybottom_water = min(y2, boxes[0][1][1])
+                        #print(xleft, xright, ytop, ybottom)
+                        if (xright_water<xleft_water) or (ybottom_water<ytop_water):
+                          area_inter_water = 0
+                        else:
+                          area_inter_water = (xright_water-xleft_water)*(ybottom_water-ytop_water)
+                        iou_water = area_inter_water/(area_cat_box+area_water_box-area_inter_water)
+
+
+                        width_foodBowl = boxes[1][1][0]-boxes[1][0][0]
+                        height_foodBowl = boxes[1][1][1]-boxes[1][0][1]
+                        area_food_box = width_foodBowl * height_foodBowl
+                        xleft_food = max(x1,boxes[1][0][0])
+                        xright_food = min(x2, boxes[1][1][0])
+                        ytop_food = max(y1, boxes[1][0][1])
+                        ybottom_food = min(y2, boxes[1][1][1])
+                        #print(xleft, xright, ytop, ybottom)
+                        if (xright_food<xleft_food) or (ybottom_food<ytop_food):
+                          area_inter_food = 0
+                        else:
+                          area_inter_food = (xright_food-xleft_food)*(ybottom_food-ytop_food)
+                        iou_food = area_inter_food/(area_cat_box+area_food_box-area_inter_food)
+
+
+                        #print(('%g ' * len(line)).rstrip() % line + '\n')'''
+
+                        #normBoxCenter = ((normBoxes[0][0][0] + normBoxes[0][1][0]) / 2, (normBoxes[0][0][1] + normBoxes[0][1][1]) / 2 )
+                        #eucledian = ((bounding_box_info[1][0] - normBoxCenter[0])**2 + (bounding_box_info[1][1] - normBoxCenter[1])**2)**(1/2)
+                        #print(eucledian)
+                        ### code ends here:
+                        print(iou_water, iou_food)
+
                         with open(txt_path + '.txt', 'a') as f:
                             f.write(('%g ' * len(line)).rstrip() % line + '\n')
 
                     if save_img or save_crop or view_img:  # Add bbox to image
                         c = int(cls)  # integer class
-                        label = None if hide_labels else (names[c] if hide_conf else f'{names[c]} {conf:.2f}')
-                        annotator.box_label(xyxy, label, color=colors(c, True))
+                        label = None if hide_labels else (names[0] if hide_conf else f'{names[0]} {conf:.2f}')
+                        annotator.box_label(xyxy, label, color=colors(0, True))
                         if save_crop:
                             save_one_box(xyxy, imc, file=save_dir / 'crops' / names[c] / f'{p.stem}.jpg', BGR=True)
 
             # Stream results
             im0 = annotator.result()
             if view_img:
+                start_point = boxes[0][0]
+                end_point = boxes[0][1]
+                color = (255, 0, 0)
+                thickness = 2
+                im0 = cv2.rectangle(im0, (boxes[0][0][0]-1,boxes[0][0][1]-35), (boxes[0][0][0]+140,boxes[0][0][1]) , (255,0,0), -1)
+                im0 = cv2.putText(im0, 'Water Bowl', (boxes[0][0][0]+5,boxes[0][0][1]-10), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (255, 255, 255), 2, cv2.LINE_AA)
+                im0 = cv2.rectangle(im0, start_point, end_point, color, thickness)
+                start_point = boxes[1][0]
+                end_point = boxes[1][1]
+                im0 = cv2.rectangle(im0, (boxes[1][0][0]-1,boxes[1][0][1]-35), (boxes[1][0][0]+130,boxes[1][0][1]) , (255,0,0), -1)
+                im0 = cv2.putText(im0, 'Food Bowl', (boxes[1][0][0]+5,boxes[1][0][1]-10), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (255, 255, 255), 2, cv2.LINE_AA)
+                im0 = cv2.rectangle(im0, start_point, end_point, color, thickness)
+                font = cv2.FONT_HERSHEY_SIMPLEX
+                org = (950, 120)
+                fontScale = 2
+                thickness = 2
+                color = (0, 255, 255)
+                if (iou_water or iou_food): im0 = cv2.putText(im0, 'Audio Model: On', org, font, fontScale, color, thickness, cv2.LINE_AA)
+                else: im0 = cv2.putText(im0, 'Audio Model: OFF', org, font, fontScale, color, thickness, cv2.LINE_AA)
+
+                #if(iou_water > iou_food):  
+                #elif(iou_food > iou_water): im0 = cv2.putText(im0, 'Eating', org, font, fontScale, color, thickness, cv2.LINE_AA)
+                
+                # Activity Index
+                frames = 15
+                if unNorm_xyxy != None:
+                    color = (255, 255, 0)
+                    perc = 0.10
+                    boundingBox_center_x = (int(unNorm_xyxy[0][0]) + int(unNorm_xyxy[0][2])) / 2
+                    boundingBox_center_y = (int(unNorm_xyxy[0][1]) + int(unNorm_xyxy[0][3])) / 2
+                    boundingBox_width = int(unNorm_xyxy[0][2]) - int(unNorm_xyxy[0][0])
+                    boundingBox_height = int(unNorm_xyxy[0][3]) - int(unNorm_xyxy[0][1])
+                    activityBox_x1 = int(boundingBox_center_x - (boundingBox_width * perc))
+                    activityBox_y1 = int(boundingBox_center_y - (boundingBox_height * perc))
+                    activityBox_x2 = int(boundingBox_center_x + (boundingBox_width * perc))
+                    activityBox_y2 = int(boundingBox_center_y + (boundingBox_height * perc))
+                    start_point = (activityBox_x1, activityBox_y1)
+                    end_point = (activityBox_x2, activityBox_y2)
+
+                    # Lagging activityBox
+                    if seen - prev_seen > frames:
+                        if prev_start_point != prev_end_point:
+                            #########
+                            # IOU between lagging & real-time activityBox
+                            # http://ronny.rest/tutorials/module/localization_001/iou/
+                            #########
+                            x1 = max(start_point[0], prev_start_point[0])
+                            y1 = max(start_point[1], prev_start_point[1])
+                            x2 = min(end_point[0], prev_end_point[0])
+                            y2 = min(end_point[1], prev_end_point[1])
+
+                            # AREA OF OVERLAP - Area where the boxes intersect
+                            width = (x2 - x1)
+                            height = (y2 - y1)
+                            # handle case where there is NO overlap
+                            area_overlap = width * height
+
+                            # COMBINED AREA
+                            area_a = (end_point[0] - start_point[0]) * (end_point[1] - start_point[1])
+                            area_b = (prev_end_point[0] - prev_start_point[0]) * (prev_end_point[1] - prev_start_point[1])
+                            area_combined = area_a + area_b - area_overlap
+
+                            # RATIO OF AREA OF OVERLAP OVER COMBINED AREA
+                            iou = 0
+                            if area_combined > 0:
+                                iou = area_overlap / area_combined
+                                print('----------', area_a, area_b, area_combined, area_overlap, iou)
+                            
+                            font = cv2.FONT_HERSHEY_SIMPLEX
+                            org = (950, 220)
+                            fontScale = 2
+                            if iou > 0: activity = 0    # low
+                            else: activity = 1          # high
+
+                        # Update lagging box
+                        prev_start_point = start_point
+                        prev_end_point = end_point
+                        prev_seen = seen
+
+                        ### CSV File Export
+                        file_exists = os.path.exists('metaData.csv')
+                        header = ['system time', 'timestamp', 'eat_drink', 'activity']
+                        data = [datetime.now(),timestamp, iou_food+iou_water, activity]
+                        timestamp += 0.5
+                        with open('metaData.csv', 'a', encoding='UTF8') as f:
+                            writer = csv.writer(f)
+                            # If file does not exists, write the header
+                            if file_exists == False: writer.writerow(header)
+                            writer.writerow(data)
+
+                    elif prev_start_point != prev_end_point:
+                        color = (255, 0, 255)
+                        im0 = cv2.rectangle(im0, prev_start_point, prev_end_point, color, thickness)
+                    # Real-time activityBox
+                    color = (255, 255, 0)
+                    im0 = cv2.rectangle(im0, start_point, end_point, color, thickness)
+
+                    font = cv2.FONT_HERSHEY_SIMPLEX
+                    org = (950, 220)
+                    fontScale = 2
+                    if activity == 0: im0 = cv2.putText(im0, 'Activity: Low', org, font, fontScale, (0, 255, 255), 2, cv2.LINE_AA)
+                    elif activity == 1: im0 = cv2.putText(im0, 'Activity: High', org, font, fontScale, (0, 255, 255), 2, cv2.LINE_AA)
+
                 cv2.imshow(str(p), im0)
                 cv2.waitKey(1)  # 1 millisecond
 
@@ -238,7 +462,7 @@ def parse_opt():
     parser.add_argument('--dnn', action='store_true', help='use OpenCV DNN for ONNX inference')
     opt = parser.parse_args()
     opt.imgsz *= 2 if len(opt.imgsz) == 1 else 1  # expand
-    print_args(vars(opt))
+    print_args(FILE.stem, opt)
     return opt
 
 
